8bb4919a71e22247717655bd2468d5856261c889
minheonjun <tamunavollt1@gmail.com>
2024-04-01 23:01:57 +0900

    add : header

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,8 +1,23 @@
-import {Outlet} from "react-router-dom";
+import {Link, Outlet} from "react-router-dom";
+import {HStack, Box, Button} from "@chakra-ui/react";
+import {FaAirbnb} from "react-icons/fa6";
+
 
 export default function Root(){
-    return <h3>
-        Root Page
-        <Outlet />
-    </h3>;
+    return (
+        <Box>
+            <HStack py={5} px={10} spacing={"2.5px"} justifyContent={"space-between"} borderBottomWidth={1} >
+                <Box color={"red.500"}>
+                    <Link to={"/"}>
+                        <FaAirbnb size={"48"} />
+                    </Link>
+                </Box>
+                <HStack spacing={2}>
+                    <Button>Log in</Button>
+                    <Button colorScheme={"red"}>Sign up</Button>
+                </HStack>
+            </HStack>
+            <Outlet />
+        </Box>
+    );
 }
