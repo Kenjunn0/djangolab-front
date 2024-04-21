8c3a1a86c825b05347ad662445cdbae3a2a54447
minheonjun <tamunavollt1@gmail.com>
2024-04-21 15:45:24 +0900

    add : render13

--- src/api.ts
+++ src/api.ts
@@ -4,7 +4,7 @@ import {QueryFunctionContext} from "@tanstack/react-query";
 import {formatDate} from "./lib/utils";
 
 const instance = axios.create({
-    baseURL: "http://127.0.0.1:8000/api/v1/",
+    baseURL: process.env.NODE_ENV === "development" ? "http://127.0.0.1:8000/api/v1/" : "https://djangolab-front.onrender.com/api/v1/",
     withCredentials: true
 });
 
